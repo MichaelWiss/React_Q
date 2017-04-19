a476ba9344d92828d3ac4983d1f2255f852cb581
MichaelWiss <michael.wiss@gmail.com>
2017-04-19 16:05:50 -0500

    example

--- app/redux.todo-example.jsx
+++ app/redux.todo-example.jsx
@@ -9,18 +9,18 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
-     switch (action.type) {
-   	case 'CHANGE_NAME':
-   	   return {
-          ...state,
-          name: action.name
-   	   };
-   	   default:
-   	     return state;
+    
+	return state;
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+	type: 'CHANGE_SEARCH_TEXT',
+	searchText: 'work'
+});
+console.log('searchText should be "work"', store.getState());
 
