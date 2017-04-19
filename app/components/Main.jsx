e5accef95c765344c001fecc8746442fc61f1373
MichaelWiss <michael.wiss@gmail.com>
2017-04-18 19:06:08 -0500

    redux

---
+++ app/components/Main.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = (props) => {
+	return(
+		<div>
+			<div className="row">
+			 <div className="large-4 medium-6 small-centered columns">
+			 <p>Main.jsx rendered</p>
+			 </div>
+		    </div>
+		</div>
+	);
+}
+
+module.exports = Main;
