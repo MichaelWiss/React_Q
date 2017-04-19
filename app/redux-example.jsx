bf981e0d45855c6acdc289f6fe50c8d0aa9b6e11
MichaelWiss <michael.wiss@gmail.com>
2017-04-19 12:03:59 -0500

    redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,7 +3,13 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 
-var reduce = (state = 'Anonymous', action) => {
+var reducer = (state = {name:'Anonymous'}, action) => {
    // state = state || {name: 'Anonymous'};
-}
+
+   return state;
+};
+
 var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
